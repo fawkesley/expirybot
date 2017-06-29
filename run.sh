befcfd5c762493bca15e1450d1fdd5bad87c1247
Paul M Furley <paul@paulfurley.com>
2017-06-29 21:15:13 +0100

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -eux
+
+THIS_SCRIPT=$0
+THIS_DIR=$(dirname ${THIS_SCRIPT})
+
+SHORT_IDS_FILE="${THIS_DIR}/data/$(date +%F)/short_key_ids.txt"
+
+
+copy_short_ids_dump_file() {
+  mkdir -p $(dirname $SHORT_IDS_FILE)
+
+  if [ ! -f "${SHORT_IDS_FILE}" ]; then
+    cp ${HOME}/short_key_ids_dump.txt "${SHORT_IDS_FILE}"
+  fi
+}
+
+make_expiring_keys_csv() {
+  ./expirybot/make_csv.py "${SHORT_IDS_FILE}"
+}
+
+send_emails() {
+  echo
+}
+
+clean_dev_shm() {
+  rm -rf /dev/shm/tmp.expirybot.*
+}
+
+copy_short_ids_dump_file
+make_expiring_keys_csv
+send_emails
+clean_dev_shm
