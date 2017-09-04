94436d9e64f43a01e441e12c87a15a800039c113
Paul M Furley <paul@paulfurley.com>
2017-09-04 08:38:07 +0100

    Exclude keys in make_keys_expiring step

--- expirybot/make_keys_expiring_csv.py
+++ expirybot/make_keys_expiring_csv.py
@@ -6,40 +6,91 @@
 - if it expires in 3 days from now, add it to the output CSV
 """
 
+import contextlib
 import datetime
 import logging
 
 from os.path import join as pjoin
 
-from .config import DATA_DIR, FINGERPRINT_CSV_HEADER
+from .config import DATA_DIR, FINGERPRINT_CSV_HEADER as CSV_HEADER
 from .utils import (
     make_atomic_csv_writer, write_key_to_csv, load_keys_from_csv,
     make_today_data_dir
 )
 
+from .exclusions import is_strong_key, is_blacklisted_domain, has_missing_email
+
 EXPIRING_DAYS = 3
+
+
+class Stats:
+    def __init__(self):
+        self.parsed_count = 0
+        self.expiring_count = 0
+        self.excluded_count = 0
 
 
 def main():
     today_data_dir = make_today_data_dir(datetime.date.today())
     setup_logging(today_data_dir)
 
-    keys_parsed_count, keys_expiring_count = (0, 0)
+    stats = Stats()
 
-    with make_atomic_csv_writer(
-            pjoin(today_data_dir, 'keys_expiring.csv'),
-            FINGERPRINT_CSV_HEADER) as csv_writer:
+    all_keys_fn = pjoin(DATA_DIR, 'keys.csv')
+    expiring_fn = pjoin(today_data_dir, 'keys_expiring.csv')
+    excluded_fn = pjoin(today_data_dir, 'keys_excluded.csv')
 
-        for key in load_keys_from_csv(pjoin(DATA_DIR, 'keys.csv')):
+    with setup_output_csvs(expiring_fn, excluded_fn) as \
+            (expiring_csv, excluded_csv):
 
-            if key.expires_in(EXPIRING_DAYS):
-                write_key_to_csv(key, csv_writer)
-                keys_expiring_count += 1
+        for key in load_keys_from_csv(all_keys_fn):
+            handle_key(key, expiring_csv, excluded_csv, stats)
 
-            keys_parsed_count += 1
+    logging.info("Checked {}, excluded {}. {} expiring in {} days.".format(
+        stats.parsed_count, stats.excluded_count, stats.expiring_count,
+        EXPIRING_DAYS
+    ))
 
-    logging.info("Checked {} keys. {} expiring in {} days.".format(
-        keys_parsed_count, keys_expiring_count, EXPIRING_DAYS))
+
+def handle_key(key, expiring_csv, excluded_csv, stats):
+    stats.parsed_count += 1
+
+    if key.expires_in(EXPIRING_DAYS):
+        if should_exclude(key):
+            stats.excluded_count += 1
+            write_key_to_csv(key, excluded_csv)
+        else:
+            write_key_to_csv(key, expiring_csv)
+            stats.expiring_count += 1
+
+
+@contextlib.contextmanager
+def setup_output_csvs(expiring_fn, excluded_fn):
+
+    with make_atomic_csv_writer(expiring_fn, CSV_HEADER) as expiring_csv, \
+         make_atomic_csv_writer(excluded_fn, CSV_HEADER) as excluded_csv:
+
+        yield (expiring_csv, excluded_csv)
+
+
+def should_exclude(key):
+    if not is_strong_key(key):
+        logging.warn("Skipping weak key: {}".format(
+            key.fingerprint))
+        return True
+
+    elif is_blacklisted_domain(key):
+        logging.warn("Skipping blacklisted domain: {}".format(
+            key.primary_email))
+        return True
+
+    elif has_missing_email(key):
+        logging.warn("Skipping key without email: {}".format(
+            key.fingerprint))
+        return True
+
+    else:
+        return False
 
 
 def setup_logging(today_data_dir):
