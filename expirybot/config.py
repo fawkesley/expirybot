04e1cbf3f6a1c23bbc9d02ec071815d490865e2e
Paul M Furley <paul@paulfurley.com>
2017-10-23 21:29:33 +0100

    Rejig config class

--- expirybot/config.py
+++ expirybot/config.py
@@ -1,30 +1,36 @@
 import io
 import json
 import logging
+import getpass
+import platform
 
 from os.path import abspath, dirname, join as pjoin
+
+import requests
 
 
 class Config():
     CONFIG_JSON = abspath(pjoin(dirname(__file__), '..', 'config.json'))
 
     def __init__(self):
-        self.keyserver = 'http://pool.sks-keyservers.net:11371'
-        self.mailgun_domain = 'example.com'
-        self.mailgun_api_key = 'INVALID-API-KEY'
-        self.blacklisted_domains = []
 
         try:
             with io.open(self.CONFIG_JSON) as f:
-                config_json = json.load(f)
-
-                self.keyserver = config_json['keyserver']
-                self.mailgun_domain = config_json['mailgun_domain']
-                self.mailgun_api_key = config_json['mailgun_api_key']
-                self.blacklisted_domains = config_json['blacklisted_domains']
+                config = json.load(f)
 
         except EnvironmentError:
             logging.warn('Failed to open {}'.format(self.CONFIG_JSON))
+            config = {}
+
+        self.mailgun_domain = config.get('mailgun_domain', 'example.com')
+
+        self.mailgun_api_key = config.get('mailgun_api_key', 'INVALID')
+
+        self.blacklisted_domains = config.get('blacklisted_domains', [])
+
+        self.keyserver = config.get(
+            'keyserver', 'http://pool.sks-keyservers.net:11371'
+        )
 
     @property
     def data_dir(self):
